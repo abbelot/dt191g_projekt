18771034b7f78daaf3b753d04b092607b25f97d4
Amanda Björk <bjk.amanda@gmail.com>
2024-02-28 13:00:25 +0100

    show posts in Home/Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,54 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using dt191g_projekt.Models;
+using dt191g_projekt.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace dt191g_projekt.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        if (_context.Posts == null)
+        {
+            return NotFound();
+        }
+        var posts = await _context.Posts.Include(p => p.Category).ToListAsync();
+        return View(posts);
     }
 
-    public IActionResult Privacy()
+    public async Task<IActionResult> Details(int? id)
     {
-        return View();
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        if (_context.Posts == null)
+        {
+            return NotFound();
+        }
+
+        var post = await _context.Posts
+            .Include(p => p.Category)
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        return View(post);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
