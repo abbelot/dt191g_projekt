a0abfeca0dc26f65223bbb39257b6c1b593c823a
Amanda Björk <bjk.amanda@gmail.com>
2024-03-06 08:56:19 +0100

    installed tailwind in project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './Pages/**/*.cshtml',
+    './Views/**/*.cshtml',
+    './Areas/Identity/**/*.cshtml'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
