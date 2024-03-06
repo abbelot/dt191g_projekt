6acf3f68da54d4572f6e061d4f06359fd5434b7b
Amanda Björk <bjk.amanda@gmail.com>
2024-03-06 14:02:01 +0100

    initial styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './Areas/Identity/**/*.cshtml'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mainColor': '#225483',
+        'btnColor': '#C35937',
+        'btnHover': '#9F4529',
+        'textColor': '#374151',
+        'footerColor': '#31313B'
+      },
+      fontFamily: {
+        'body': ['Kantumruy', 'sans-serif'],
+        'heading': ['Karma', 'serif']
+      },
+    },
   },
   plugins: [],
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    navToggle.addEventListener('click', function () {
+        // Toggle active class that controls visibility
+        navMenu.classList.toggle('active');
+    });
+});
